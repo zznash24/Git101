5b58bcb93ca348368bbaf18e0c32090bdead4ccc
Zach Nash <afreakenturtle@gmail.com>
2021-12-23 18:55:40 -0500

    hello-world.js

---
+++ hello-world.js
@@ -0,0 +1,20 @@
+console.log("hello world")
+
+// var object = {
+//     Testing: 4,
+//     testing2: "hello",
+//     something: "just because"
+// };
+
+// console.log (object["something"])
+
+// var number1 = 5;
+// var number2 = 2;
+
+// console.log (number1 = 5 + 5) 
+
+// function add(number1, number2){
+//     console.log(number1 + number2)
+// }
+
+// add()
