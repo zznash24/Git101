203a37c1d9de73e2d5c092e7a9fcaea4e98e4002
Zach Nash <afreakenturtle@gmail.com>
2021-12-23 20:58:13 -0500

    hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -35,67 +35,61 @@
 // people >4 < 6: 15%
 // people >6 < 10: 20%
 // people >10: 25%
-function totalWithTip(items) {
-    let subtotal = 0;
-    let tip_rate = 0;
-    let people = 0;
-
-
-  
-    for (let i = 0; i < items.length; i = i + 1) {
-      subtotal = subtotal + items[i]
-    }
-  
-    if (subtotal < 100) {
-      tip_rate = 0.1;
-    } else if (subtotal < 200) {
-      tip_rate = 0.2;
-    } else if (subtotal < 300) {
-      tip_rate = 0.3;
-    } else {
-      tip_rate = 0.5
-    }
-  
-    const tip = subtotal * tip_rate;
-    return subtotal + tip;
-  }
-  
-  let total = totalWithTip([5, 10, 15, 20, 100, 500]);
-
-
-
-
-
-
-
-
-
-// people < 4 tip : 10%
-// people >4 < 6: 15%
-// people >6 < 10: 20%
-// people >10: 25%
 
 // function totalWithTip(items) {
 //     let subtotal = 0;
 //     let tip_rate = 0;
-//     let people = 0;
+//     let people = item.length;
   
 //     for (let i = 0; i < items.length; i = i + 1) {
 //       subtotal = subtotal + items[i]
 //     }
   
-//     if (subtotal < 4) {
+//     if (people < 4) {
 //       tip_rate = 0.1;
-//     } else if (subtotal < 4, subtotal >6) {
-//       tip_rate = 0.2;
-//     } else if (subtotal < 6, subtotal >10) {
+//     } else if (4 >= people && people < 6) {
+//       tip_rate = 0.15;
+//     } else if (people < 10) {
 //       tip_rate = 0.2;
 //     } else {
-//       tip_rate = 0.5;
+//       tip_rate = 0.25
 //     }
   
 //     const tip = subtotal * tip_rate;
 //     return subtotal + tip;
 //   }
   
-//   let total = totalWithTip([1, 2, 3, 4, 5]);
+//   let total = totalWithTip([5, 10, 15, 20, 100, 500]); //item.length => 5
+  
+
+function makeANumber() {
+let myNum = 7708675309;
+return myNum;
+
+}
+
+function makeAnInteger(){
+let myInt = 23;
+return myInt;
+}
+
+function makeaFloat(){
+let myFloat = 16.25;
+return myFloat;
+}
+
+function makeZero() {
+    let zilch = 0;
+    return zilch;
+}
+
+function makeNothing() {
+    let huh;
+    return huh;
+}
+
+function makeBoolean() {
+    let myBool = true;
+    return myBool;
+}
+
