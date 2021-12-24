d655c717b426ca7c57054262626b02c6988b3fda
Zach Nash <afreakenturtle@gmail.com>
2021-12-23 23:02:30 -0500

    hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -166,7 +166,7 @@ function multipleTypes() {
 
 function indexAccess() {
     let people = [`jack`, `jill`, `zach`, `andy`, `shannon`, `kyler`, `monique`, `Jaun`];
-    
+    return people[3];
 }
-console.log (indexAccess[3]);
+console.log (indexAccess());
 
